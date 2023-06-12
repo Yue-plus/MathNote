3a926f41c45aa54ac84390d444b11b0328e8f0eb
Yue-plus <Yue_plus@foxmail.com>
2023-06-12 15:41:43 +0800

    update Index 本人的数学和物理作业可能包含以下内容

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import ContributorsList from "../components/ContributorsList";
+import { useColorMode } from '@docusaurus/theme-common';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -23,14 +24,26 @@ function HomepageHeader() {
   );
 }
 
+function Img() {
+  const { colorMode } = useColorMode();
+  return <img
+    src="/img/本人的数学和物理作业可能包含以下内容.png"
+    style={{
+      display: "block",
+      margin: "0 auto",
+      filter: colorMode === "dark" ? "invert(1)" : "",
+    }}
+  />
+}
+
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
       title={`首页`}
       description="MathNote - 数学笔记">
       <HomepageHeader />
       <main>
+        <Img />
         <ContributorsList />
         {/*<HomepageFeatures />*/}
       </main>
